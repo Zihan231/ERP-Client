fba409e1d006399ce090c993754730f47b9878ac
Zihan231 <Zihanislam231@gmail.com>
2026-01-10 12:36:08 +0600

    still ok

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -24,9 +24,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* <NavBar></NavBar> */}
         {children}
-        {/* <Footer></Footer> */}
 
       </body>
     </html>
